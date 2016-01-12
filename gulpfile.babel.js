bd28a1058a60cbf9595c31908def41a2d571b9a3
Mike Green <mike.is.green@gmail.com>
2016-01-12 08:38:06 -0500

    Initial commit - JS compilation works for main module

---
+++ gulpfile.babel.js
@@ -0,0 +1,47 @@
+import gulp        from 'gulp';
+import plugins     from 'gulp-load-plugins';
+import runSequence from 'run-sequence';
+import path        from 'path';
+import browserify  from 'browserify';
+import babelify    from 'babelify';
+import source      from 'vinyl-source-stream';
+
+const $         = plugins();
+const copyFiles = [
+  'source/**/*',
+  '!source/js/**/*.{js,jsx}',
+  '!source/css/**/*.scss'
+];
+
+gulp.task('scripts:main', cb => {
+  let bundler = browserify({
+    entries: './source/js/main.js',
+    extensions: [ '.js', '.jsx' ],
+    debug: true
+  })
+  .transform(babelify);
+
+  return bundler.bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('scripts', [ 'scripts:main' ]);
+
+gulp.task('styles', () => {
+  return gulp.src('./source/css/**/*.scss')
+    .pipe($.sass({
+      outputStyle: 'expanded',
+      includePath: [
+        'source/css',
+        path.join(__dirname, 'node_modules')
+      ]
+    }))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('copy', () => gulp.src(copyFiles).pipe(gulp.dest('build')));
+
+gulp.task('default', cb => {
+  runSequence('copy', [ 'scripts', 'styles' ], cb);
+});
