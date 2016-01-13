59be01f45873ac01fcaeb0850607de84da385242
Mike Green <mike.is.green@gmail.com>
2016-01-13 14:26:42 -0500

    Binding logger config into application state

--- source/js/lib/application.jsx
+++ source/js/lib/application.jsx
@@ -4,13 +4,30 @@ import React from 'react';
 class Application extends React.Component {
   constructor() {
     super();
+    this.state = {
+      loggerStatus: this.props.status
+    };
   }
 
   render() {
+    if (!this.state.loggerStatus) {
+      return (
+        <div className="application">
+          <p>
+            <em>Logger instance not found</em>
+          </p>
+        </div>
+      );
+    }
+
     return (
       <div className="application"></div>
     );
   }
 }
+
+Application.propTypes = {
+  loggerStatus: React.PropTypes.object
+};
 
 export default Application;
