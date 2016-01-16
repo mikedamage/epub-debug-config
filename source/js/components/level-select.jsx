49fb13f71a06b857c90246e90d22009f360ea0ac
Mike Green <mike.is.green@gmail.com>
2016-01-16 13:30:53 -0500

    Add level select input element

--- source/js/components/level-select.jsx
+++ source/js/components/level-select.jsx
@@ -1,3 +1,4 @@
+import _     from 'lodash';
 import React from 'react';
 
 class LevelSelect extends React.Component {
@@ -5,9 +6,31 @@ class LevelSelect extends React.Component {
     super(props);
   }
 
+  handleChange(evt) {
+    console.log(evt);
+  }
+
   render() {
+    let opts = {
+      'debug' : 'Debug',
+      'info'  : 'Info',
+      'warn'  : 'Warn',
+      'error' : 'Error'
+    };
+
+    opts = _.map(opts, (label, val) => {
+      return (
+        <option key={val} value={val}>{label}</option>
+      );
+    });
+
     return (
-      <div className="level-select"></div>
+      <div className="level-select">
+        <select
+          className="level-select__input"
+          name="level"
+          onChange={this.handleChange}>{opts}</select>
+      </div>
     );
   }
 }
